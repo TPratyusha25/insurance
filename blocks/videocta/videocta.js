8d3dc4c70e187627b09b9126be7a8cf73eea4d1c
TPratyusha25 <tpratyusha@acg.aaa.com>
2025-06-26 00:43:25 +0530

    Update videocta.js

--- blocks/videocta/videocta.js
+++ blocks/videocta/videocta.js
@@ -1,28 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
   const wrapper = document.querySelector(".videocta-wrapper");
-
   if (!wrapper) return;
 
   const img = wrapper.querySelector("picture img");
-  const videoLink = wrapper.querySelector("a[href*='youtube.com']");
+  const link = wrapper.querySelector("a[href*='youtube.com']");
 
-  if (!img || !videoLink) return;
+  if (!img || !link) {
+    console.warn("Thumbnail image or YouTube link not found.");
+    return;
+  }
 
-  const youtubeUrl = videoLink.getAttribute("href");
-  const embedUrl = youtubeUrl.replace("watch?v=", "embed/") + "?autoplay=1";
+  const embedUrl = link.href.replace("watch?v=", "embed/") + "?autoplay=1";
+
+  img.style.cursor = "pointer";
 
   img.addEventListener("click", () => {
     const iframe = document.createElement("iframe");
-    iframe.setAttribute("src", embedUrl);
+    iframe.src = embedUrl;
+    iframe.width = "100%";
+    iframe.height = "100%";
     iframe.setAttribute("frameborder", "0");
     iframe.setAttribute("allowfullscreen", "");
     iframe.setAttribute("allow", "autoplay; encrypted-media");
-    iframe.style.width = "100%";
-    iframe.style.height = "100%";
 
     const picture = img.closest("picture");
-    if (picture && picture.parentNode) {
-      picture.parentNode.replaceChild(iframe, picture);
+    if (picture && picture.parentElement) {
+      picture.parentElement.replaceChild(iframe, picture);
+    } else {
+      console.error("Picture element not found.");
     }
   });
 });
