cebba6029507d8e37edbaac1839e4c6d029d6487
TPratyusha25 <tpratyusha@acg.aaa.com>
2025-06-27 04:53:40 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,6 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-    const searchText = document.querySelector(".header > div:nth-child(7) > div > p");
+    const searchText = document.querySelector('.header > div:nth-child(7) > div > p');
 
     if (searchText) {
       searchText.addEventListener("click", function () {
