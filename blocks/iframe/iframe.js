bb4f213f0475446d1b3b2213245609c7393bfd03
TPratyusha25 <tpratyusha@acg.aaa.com>
2025-06-26 15:19:53 +0530

    Update iframe.js

--- blocks/iframe/iframe.js
+++ blocks/iframe/iframe.js
@@ -1,13 +1,39 @@
-function playVideo(imgElement) {
-  const container = imgElement.closest('.embed-container');
-  const iframe = container.querySelector('iframe');
-  container.classList.add('playing');
-  iframe.contentWindow.postMessage(
-    JSON.stringify({
-      event: "command",
-      func: "playVideo",
-      args: []
-    }),
-    "*"
-  );
+const iframeData = {
+  image: "/assets/Images/Careers/career-ea-video-overlay.jpg",
+  imageAlt: "Video",
+  videoUrl: "https://www.youtube.com/embed/6XT_TMEJR7Q"
+};
+
+function createIframeComponent(data) {
+  const container = document.createElement("div");
+  container.className = "embed-container";
+
+  const img = document.createElement("img");
+  img.src = data.image;
+  img.alt = data.imageAlt;
+  img.onclick = () => {
+    container.classList.add("playing");
+    iframe.contentWindow.postMessage(
+      JSON.stringify({
+        event: "command",
+        func: "playVideo",
+        args: []
+      }),
+      "*"
+    );
+  };
+
+  const iframe = document.createElement("iframe");
+  iframe.src = `${data.videoUrl}?rel=0&enablejsapi=1&origin=${window.location.origin}`;
+  iframe.title = "YouTube Video";
+  iframe.allowFullscreen = true;
+  iframe.frameBorder = "0";
+
+  container.appendChild(img);
+  container.appendChild(iframe);
+  document.body.appendChild(container);
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  createIframeComponent(iframeData);
+});
