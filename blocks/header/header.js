b7e201878e0ad678e3540e22216aa0a9e3e065b0
TPratyusha25 <tpratyusha@acg.aaa.com>
2025-06-27 04:24:55 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,1 +1,11 @@
 
+  document.addEventListener("DOMContentLoaded", function () {
+    const searchText = document.querySelector('.header > div:nth-child(7) > div > p ');
+
+    // Make the <p> editable when clicked
+    searchText.addEventListener("click", function () {
+      searchText.setAttribute("contenteditable", "true");
+      searchText.focus();
+    });
+  });
+
