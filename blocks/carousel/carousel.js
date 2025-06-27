3aecac0c305d66f1ad5b065478c290bdbb54fb91
TPratyusha25 <tpratyusha@acg.aaa.com>
2025-06-27 21:26:04 +0530

    Update carousel.js

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,1 +1,23 @@
+ let currentSlide = 0;
+  const ul = document.querySelector('.carousel.block > div:nth-child(6) ul');
+  const itemWidth = 308; // your new min-width
+  const visibleItems = 3;
 
+  document.querySelector('.carousel.block > div:nth-child(2)').addEventListener('click', () => {
+    if (currentSlide > 0) {
+      currentSlide--;
+      updateSlider();
+    }
+  });
+
+  document.querySelector('.carousel.block > div:nth-child(4)').addEventListener('click', () => {
+    const maxSlides = ul.children.length - visibleItems;
+    if (currentSlide < maxSlides) {
+      currentSlide++;
+      updateSlider();
+    }
+  });
+
+  function updateSlider() {
+    ul.style.transform = `translateX(-${currentSlide * itemWidth}px)`;
+  }
