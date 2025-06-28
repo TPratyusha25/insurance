3465af858e2e5fd13481827765a6695c3569cce0
TPratyusha25 <tpratyusha@acg.aaa.com>
2025-06-29 00:14:42 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -2,21 +2,11 @@ function enhancePage() {
   // Wait for editable paragraph
   waitForElement('.header > div:nth-child(7) > div > p', (editableP) => {
     editableP.setAttribute("contenteditable", "true");
-    editableP.style.cursor = "text";
-    editableP.style.outline = "none";
-    editableP.style.border = "2px solid #d40000";
     editableP.addEventListener("click", () => editableP.focus());
   });
 
   // Wait for button-like paragraph
   waitForElement('.header > div:nth-child(4) > div > p', (joinBtn) => {
-    joinBtn.style.cursor = "pointer";
-    joinBtn.style.backgroundColor = "#2860a2";
-    joinBtn.style.color = "#fff";
-    joinBtn.style.padding = "10px 20px";
-    joinBtn.style.borderRadius = "4px";
-    joinBtn.style.display = "inline-block";
-    joinBtn.style.textAlign = "center";
 
     joinBtn.addEventListener("click", () => {
       window.location.href = 'https://memberapps.acg.aaa.com/membership-join/classic/member-details?...';
