e91eda7ccdee0335f3283a0a97d2c0bc0946c338
TPratyusha25 <tpratyusha@acg.aaa.com>
2025-06-30 01:50:29 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -5,24 +5,6 @@ function enhancePage() {
     editableP.style.outline = "none";
     editableP.addEventListener("click", () => editableP.focus());
   });
-
-  // Wait for button-like paragraph
- waitForElement('.header > div:nth-child(4) > div > p', (joinBtn) => {
-  joinBtn.style.cursor = "pointer";
-
-  // Hover effect
-  const originalBg = joinBtn.style.backgroundColor;
-  joinBtn.addEventListener("mouseenter", () => {
-    joinBtn.style.backgroundColor = "#e40606"; // red on hover
-  });
-  joinBtn.addEventListener("mouseleave", () => {
-    joinBtn.style.backgroundColor = originalBg; // revert to original
-  });
-
-  joinBtn.addEventListener("click", () => {
-    window.location.href = 'https://memberapps.acg.aaa.com/membership-join/classic/member-details?adobe_mc_sdid=SDID%3D0990E315639EB1BE-66805E249D4554CC%7CMCORGID%3D0CD9210654E6C1AD0A4C98A7@AdobeOrg%7CTS%3D1751134081&adobe_mc_ref=https:%2F%2Fmichigan.aaa.com%2Fcareers%2Fentrepreneurial-agent.aspx&mboxSession=f0a0eb0b9f804aec92c2777cfb4d560d';
-  });
-});
 }
 
 // Wait for DOM and page show
